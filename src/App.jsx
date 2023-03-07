f2a97e0a9f99210496ffacd6333864144a9d5f92
Sabbir Ahmed <39632780+Sabbirmadman@users.noreply.github.com>
2023-03-07 18:35:50 +0600

    name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+    Hero,
+    Navbar,
+    About,
+    Tech,
+    Experience,
+    Works,
+    Feedbacks,
+    Contact,
+    EarthCanvas,
+    BallCanvas,
+    ComputersCanvas,
+    StarsCanvas,
+} from "./components";
+
 function App() {
-    return <div className="App">APp</div>;
+    return (
+        <Router>
+            <div className="relative z-0 bg-primary">
+                <div
+                    className="bg-hero-pattern
+                
+                
+                bg-cover bg-no-repeat bg-center"
+                >
+                    <Navbar />
+                    <Hero />
+                </div>
+                <About />
+                <Experience />
+                <Tech />
+                <Works />
+                <Feedbacks />
+                <div className="relative z-0">
+                    <Contact />
+                    <StarsCanvas />
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
